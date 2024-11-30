c4f5a9238c9079d15bfd31da79af5d434a2f61cc
João Fernandes <joaofergear003@gmail.com>
2024-11-30 12:29:33 -0300

    nvim: update options file

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,51 +1,80 @@
 -- See `:help options`
--- Netrw explorer settings
-vim.g.netrw_keepdir = 0;
-vim.g.netrw_winsize = 20;
-vim.g.netrw_liststyle = 0;
-vim.g.netrw_banner = 0;
 
--- Make the cursor block shaped and blink
+-- Netrw settings
+vim.g.netrw_banner = 0;
+vim.g.netrw_preview = 1;
+vim.g.netrw_liststyle = 3;
+vim.g.netrw_winsize = 30;
+
+-- Make the cursor block shaped and blink.
 vim.opt.guicursor = [[a:block-blinkwait700-blinkoff400-blinkon250-Cursor/lCursor]]
 
---vim.opt.list = true;
---vim.opt.listchars:append("space:·")
-vim.opt.laststatus = 3
-vim.opt.wrap = false;                            -- Don't wrap lines
-vim.opt.termguicolors = true;                    -- Termguicolors
-vim.opt.number = true;                           -- Display line number
-vim.opt.relativenumber = true;                   -- Display relative line number
-vim.opt.backup = false;                          -- Don't create backup files
-vim.opt.fileencoding = "utf-8";                  -- File encoding
-vim.opt.clipboard:append("unnamedplus");         -- Allow neovim to access system clipboard
-vim.opt.completeopt = { "menuone", "noselect" }; -- Complete opt
-vim.opt.hlsearch = true;                         -- Keep highlight on after search
-vim.opt.incsearch = true;                        -- Incremental search
-vim.opt.ignorecase = true;                       -- Ignore case on searching
-vim.opt.mouse = "a";                             -- Allow mouse for all modes
-vim.opt.showmode = false;                        -- Do not display current mode
-vim.opt.smartcase = true;                        -- Smart case
-vim.opt.smartindent = true;                      -- Aware indentations for LF inserts
-vim.opt.expandtab = true;                        -- Use spaces for indenting
-vim.opt.tabstop = 8;                             -- Num of space characters per tab
-vim.opt.shiftwidth = 8;                          -- Space per indentation level
-vim.opt.autoindent = true;                       -- Autoindent
-vim.opt.splitbelow = true;                       -- Split window below
-vim.opt.splitright = true;                       -- Split window right
-vim.opt.swapfile = false;                        -- Do not create swap file
-vim.opt.undofile = true;                         -- Enable persistent undo tree
-vim.opt.timeoutlen = 1000;                       -- Delay to break a map sequence (in ms)
-vim.opt.updatetime = 50;                         -- Delay to start completion (in ms)
-vim.opt.writebackup = true;                      -- Enable backup file
--- vim.opt.colorcolumn = {"72", "80"};              -- Draw column boundaries at ranges
-vim.opt.signcolumn = "yes";                      -- Draw signs at columns when available
-vim.opt.scrolloff = 999;                         -- Always center the cursor line when scrolling vertically
-vim.opt.sidescrolloff = 999;                     -- 8 column room left when scrolling horizontally
+-- Enables just one status line across all windows.
+vim.opt.laststatus = 3;
 
-vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter", "WinEnter"}, {
-        command = "set cursorline"
-})
+vim.opt.number = true;
+vim.opt.relativenumber = true;
 
-vim.api.nvim_create_autocmd({"BufLeave", "BufWinLeave", "WinLeave"}, {
-        command = "set nocursorline"
-})
+vim.opt.termguicolors = true;
+
+-- Create backup files?
+vim.opt.backup = false;
+
+vim.opt.fileencoding = "utf-8";
+
+-- Use system clipboard instead registers.
+vim.opt.clipboard:append("unnamedplus");
+
+-- Keep search hightlighted?
+vim.opt.hlsearch = true;
+-- Match search in real time typing?
+vim.opt.incsearch = true;
+-- Ignore case on searching?
+vim.opt.ignorecase = true;
+
+-- Display current mode?
+vim.opt.showmode = false;
+
+-- Allow mouse for all modes.
+vim.opt.mouse = "a";
+
+-- Use spaces for indenting?
+vim.opt.expandtab = true;
+-- Num of space chars a tab is equivalent.
+vim.opt.tabstop = 8;
+-- Num of space chars to be inserted.
+vim.opt.shiftwidth = 8;
+
+vim.opt.smartcase = true;
+vim.opt.smartindent = true;
+vim.opt.autoindent = true;
+
+vim.opt.splitbelow = true;
+vim.opt.splitright = true;
+
+-- Create swap files?
+vim.opt.swapfile = false;
+
+-- Enable persistent undo tree?
+vim.opt.undofile = true;
+
+-- Delay to break a map sequence (in ms).
+vim.opt.timeoutlen = 1000;
+
+-- Delay to start completion (in ms).
+vim.opt.updatetime = 50;
+
+-- Prevents the file being lost if vim fails to write the buffer.
+vim.opt.writebackup = true;
+
+-- Highlight the cursorline.
+vim.opt.cursorline = true;
+
+-- Draw delimiter at the given column ranges.
+vim.opt.colorcolumn = {"72", "80"};
+
+-- Draw signs at columns when available.
+vim.opt.signcolumn = "yes"; -- (yes = always)
+
+vim.opt.scrolloff = 999;
+vim.opt.sidescrolloff = 4;
