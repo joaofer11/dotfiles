6ec7f95fee5d594fb8a22c0850233c2bc2bce26b
João Fernandes <joaofergear003@gmail.com>
2025-01-14 15:51:03 -0300

    nvim: change leap plugin keymaps

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -2,6 +2,8 @@ return {
 	"ggandor/leap.nvim",
 	dependencies = {"tpope/vim-repeat"},
 	config = function()
-		require("leap").create_default_mappings();
+		vim.keymap.set("n",        "s", "<Plug>(leap)")
+		vim.keymap.set({"x", "o"}, "s", "<Plug>(leap-forward)")
+		vim.keymap.set({"x", "o"}, "x", "<Plug>(leap-backward)")
 	end
 }
