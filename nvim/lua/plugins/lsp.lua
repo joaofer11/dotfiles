9dccf7abf2cdb88e9b13a3d9a400baf4c1e49bf4
João Fernandes <joaofergear003@gmail.com>
2025-01-30 19:52:24 -0300

    nvim: set up tsserver LSP

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -13,7 +13,13 @@ return {
 	"neovim/nvim-lspconfig",
 	dependencies = "hrsh7th/cmp-nvim-lsp",
 	config = function()
+		local lspconfig = require("lspconfig")
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-		require("lspconfig").clangd.setup({ capabilities = capabilities })
+
+		lspconfig.clangd.setup({capabilities = capabilities})
+		lspconfig.ts_ls.setup({
+			capabilities = capabilities,
+			filetypes = {"javascript", "typescript"}
+		})
 	end
 }
